3b2f57f233579cd8d4669f2fa310cb97e7c4b875
mdewinged <alexandreamk1@gmail.com>
2022-02-02 20:20:42 -0300

    .

--- Game.cpp
+++ Game.cpp
@@ -8,15 +8,14 @@ class Game{
   private:
     Game(string title, int width, int height);
     static Game* instance;
-    SDL_Window* window;
-    SDL_Renderer* renderer;
-    State& state;
+    //SDL_Window* window;
+    //SDL_Renderer* renderer;
+    //State& state;
   public:
-    Game(Game& other) = delete;
     ~Game();
     void run();
-    SDL_Renderer* GetRenderer();
-    State& GetState();
+    //SDL_Renderer* GetRenderer();
+    //State& GetState();
     static Game& GetInstance();
   protected:
   
